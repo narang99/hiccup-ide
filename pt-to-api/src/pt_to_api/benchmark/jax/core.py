11998b9301767b907a434749547847037ac28c05
narang99 <hariom2711@gmail.com>
2026-06-05 19:33:40 +0530

    fat encoder

--- pt-to-api/src/pt_to_api/benchmark/jax/core.py
+++ pt-to-api/src/pt_to_api/benchmark/jax/core.py
@@ -1,7 +1,9 @@
-from flax import nnx
-import jax
 from typing import NamedTuple
+
+import jax
 import jax.numpy as jnp
+from flax import nnx
+
 from pt_to_api.benchmark.core import SingleRun
 
 
@@ -14,13 +16,28 @@ class ModelTrainStepUnconditionalParams(NamedTuple):
     epoch_mod: int
 
 
+# class Autoencoder(nnx.Module):
+#     def __init__(self, input_dim, n_components, rngs):
+#         self.encoder = nnx.Linear(input_dim, n_components, rngs=rngs, use_bias=False)
+#         self.decoder = nnx.Linear(n_components, input_dim, rngs=rngs, use_bias=False)
+
+#     def __call__(self, x):
+#         codes = self.encoder(x)
+#         latent = codes[..., None] * self.decoder.kernel[None, ...]
+#         recon = jnp.sum(latent, axis=1)
+#         latent_perm = jnp.transpose(latent, (0, 2, 1))  # [batch, columns, components]
+#         return recon, codes, latent_perm
+
+
 class Autoencoder(nnx.Module):
     def __init__(self, input_dim, n_components, rngs):
-        self.encoder = nnx.Linear(input_dim, n_components, rngs=rngs, use_bias=False)
+        self.encoder = nnx.Linear(input_dim, 20, rngs=rngs, use_bias=False)
+        self.fat = nnx.Linear(20, n_components, rngs=rngs, use_bias=False)
         self.decoder = nnx.Linear(n_components, input_dim, rngs=rngs, use_bias=False)
 
     def __call__(self, x):
-        codes = self.encoder(x)
+        codes = self.fat(nnx.relu(self.encoder(x)))
+
         latent = codes[..., None] * self.decoder.kernel[None, ...]
         recon = jnp.sum(latent, axis=1)
         latent_perm = jnp.transpose(latent, (0, 2, 1))  # [batch, columns, components]
