edb7039addb2fc9f1b7960295c5fb9789a8480f5
narang99 <hariom2711@gmail.com>
2026-06-17 19:42:26 +0530

    encode keyts

--- olt/src/olt/stages/s2_collect_patches.py
+++ olt/src/olt/stages/s2_collect_patches.py
@@ -230,7 +230,7 @@ class PatchExtractor:
                         "__key__": str(uuid4()),
                         "patch.pth": tensor_to_bytes(channels_patches),
                         "indices.pth": tensor_to_bytes(channels_indices),
-                        "input_keys": json.dumps(ordered_keys).encode("utf-8"),
+                        "input_keys.json": json.dumps(ordered_keys).encode("utf-8"),
                     }
                 )
 
