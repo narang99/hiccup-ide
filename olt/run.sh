1f3f9618ae9cdbb7d293ffa8d5391a1c87a1cc0a
narang99 <hariom2711@gmail.com>
2026-07-16 09:17:45 +0530

    add all notebooks

---
+++ olt/run.sh
@@ -0,0 +1,7 @@
+uv run scripts/fvis.py notebooks/this-and-prev/main_df.csv notebooks/this-and-prev/feature-viz-inp-batches/2.json notebooks/this-and-prev/feature-vis-bulk
+uv run scripts/fvis.py notebooks/this-and-prev/main_df.csv notebooks/this-and-prev/feature-viz-inp-batches/3.json notebooks/this-and-prev/feature-vis-bulk
+uv run scripts/fvis.py notebooks/this-and-prev/main_df.csv notebooks/this-and-prev/feature-viz-inp-batches/4.json notebooks/this-and-prev/feature-vis-bulk
+uv run scripts/fvis.py notebooks/this-and-prev/main_df.csv notebooks/this-and-prev/feature-viz-inp-batches/5.json notebooks/this-and-prev/feature-vis-bulk
+uv run scripts/fvis.py notebooks/this-and-prev/main_df.csv notebooks/this-and-prev/feature-viz-inp-batches/6.json notebooks/this-and-prev/feature-vis-bulk
+uv run scripts/fvis.py notebooks/this-and-prev/main_df.csv notebooks/this-and-prev/feature-viz-inp-batches/7.json notebooks/this-and-prev/feature-vis-bulk
+uv run scripts/fvis.py notebooks/this-and-prev/main_df.csv notebooks/this-and-prev/feature-viz-inp-batches/8.json notebooks/this-and-prev/feature-vis-bulk
